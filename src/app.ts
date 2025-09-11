019bc212db2cea0bd97f549f87d968d42b56dd8f
silveriale <leticia.silveria.costa@gmail.com>
2025-09-11 16:39:34 -0300

    Habilitando Cors
    -  importa o CORS no arquivo app.test e o inicializa com um simples app.use(). Com isso, a API já ficou pronta para lidar com requisições de diferentes origens.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-// Este arquivo configura a aplicação Express (middlewares e rotas base), e exporta a instância do app para que outro arquivo (ex.: server.ts com o app.listen).
+// Este arquivo configura a aplicação Express, adicionando middlewares globais (como JSON e CORS), e exporta a instância do app para que outro arquivo (ex.: server.ts com o app.listen).
 
 import express from "express";
+import cors from "cors"; // Importa o middleware CORS para permitir requisições de outros domínios
 
 const app = express(); // Cria a instância da aplicação Express
 
 app.use(express.json()); // Middleware: permite que o servidor entenda requisições com corpo JSON
+app.use(cors()); //  Middleware: habilita CORS, permitindo que clientes externos acessem a API
 
 app.get("/", (request, response) => {
   // Define uma rota GET para o caminho raiz "/"
